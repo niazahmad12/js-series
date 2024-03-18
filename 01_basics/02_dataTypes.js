c00775944af41b9e0443fc937c0cb98b4e678d28
niazahmad12 <51156528+niazahmad12@users.noreply.github.com>
2024-03-18 10:09:00 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict" ; // treat all js code as newer version
+
+//alert('3+3') we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readbility should be high work but not good practice
+
+    //javascript documentation to view all rules and regulations visit
+    //1- ECMA International  (https://tc39.es/ecma262/)
+    //2- CDN 
+
+    let age=18
+    // data types
+     // number => 2 to power 53
+     //  bigint
+     // string =>""
+     // boolean => true /false
+     // null => standalone value
+     // undefined =>
+     // symbol => unique
+
+
+     // object
+
+     console.log(typeof "Niaz Ahmad");
+     console.log(typeof age);
+     console.log(typeof null); // object
+     console.log(typeof undefined); // undefined
+
+     
